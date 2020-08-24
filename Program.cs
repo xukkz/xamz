ad60a60b11d71ee5b9f1ec22b57182cca87c8f8f
Wiggo <wiggo.bergman@gmail.com>
2020-08-24 20:40:12 +0200

    First commit!

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void drawThousand()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(i + 1);
+            }
+        }
+        static void modoThree()
+
+        {
+            bool modu = false;
+            int x;
+            x = 9;
+            if ((x % 3) == 0)
+            {
+                modu = true;
+            }
+            Console.WriteLine(modu);
+        }
+        static void Main()
+        {
+            drawThousand();
+            modoThree();
+            Console.ReadLine();
+        }
+    }
+}
+
